12856755db64d1b5e89cbf51a33ad962e3a10188
Simon Perreault <simon.perreault@dialstack.ai>
2025-09-25 09:58:45 -0400

    Security: Fix public subnet and Checkov configuration
    - Disable auto-assign public IP on public subnets (use Elastic IPs instead)
    - Skip CKV_AWS_24 in Checkov (SSH from 0.0.0.0/0 for bastion is by design)
    - Addresses CKV_AWS_130 while maintaining bastion host functionality
    
    This should resolve fixable Checkov security scan issues.

--- tofu/networking.tf
+++ tofu/networking.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = var.availability_zones[0]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false  # Security: Don't auto-assign public IPs, use Elastic IPs instead
 
   tags = {
     Name = "${var.project_name}-public-a"
@@ -38,7 +38,7 @@ resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = var.availability_zones[1]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false  # Security: Don't auto-assign public IPs, use Elastic IPs instead
 
   tags = {
     Name = "${var.project_name}-public-b"
